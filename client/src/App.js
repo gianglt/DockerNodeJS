0e086311573cd1f7580ea55c9bec72c95a4f00d7
gianglt <gianglt@gmail.com>
2021-10-02 22:47:00 +0700

    Học với chả hành

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem,Container } from 'r
 
 import AppNavbar from './components/AppNavbar';
 import AppCarousel from './components/AppCarousel'; 
-// import BannerExample from './components/AppBanner';
+
 
 
 export default function App() {
--- client/src/components/AppCarousel.js
+++ client/src/components/AppCarousel.js
@@ -1,40 +1,78 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel'
-import { Paper, Button } from '@material-ui/core'
+// import { Paper, Button } from '@material-ui/core'
 
-function AppCarousel(props)
+import { Image } from 'react-bootstrap';
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import '../css/App.css';
+
+const items = [
+    {
+        name: "Random Name #1",
+        description: "Probably the most random thing you have ever seen!",
+        image: "http://static.asiawebdirect.com/m/bangkok/portals/vietnam/homepage/da-nang/pagePropertiesImage/da-nang-vietnam.jpg"
+    },
+    {
+        name: "Random Name #2",
+        description: "Hello World!",
+        image: "http://static.asiawebdirect.com/m/bangkok/portals/vietnam/homepage/da-nang/first-time/pagePropertiesImage/da-nang.jpg"
+        
+    }
+];
+
+function AppCarousel (props)
 {
-    var items = [
-        {
-            name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
-        },
-        {
-            name: "Random Name #2",
-            description: "Hello World!"
-        }
-    ]
 
-    return (
-        <Carousel>
-            {
-                items.map( (item, i) => <Item key={i} item={item} /> )
-            }
-        </Carousel>
-    )
+        return (
+            <Carousel>
+                {
+                    items.map((item, i) => <Item key={i} item={item} />)
+                }
+            </Carousel>
+        );
+
 }
 
 function Item(props)
 {
     return (
-        <Paper>
-            <h2>{props.item.name}</h2>
-            <p>{props.item.description}</p>
 
-            <Button className="CheckButton">
-                Check it out!
-            </Button>
-        </Paper>
+        <Card sx={{ maxWidth: 1600 }} className="imgWide">
+            <CardMedia
+                component="img"
+                alt="green iguana"
+                height="400"
+                image={props.item.image}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                {props.item.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                {props.item.description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+
+        // <Paper>
+        //     <h2>{props.item.name}</h2>
+        //     <p>{props.item.description}</p>
+
+        //     <Button className="CheckButton">
+        //         Check it out!
+        //     </Button>
+        //     <Image src={props.item.image} thumbnail/>
+        // </Paper>
     )
 }
 
